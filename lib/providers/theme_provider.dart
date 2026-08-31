efc2259660f786eac18c36d08dc17d1c085f59cb
Dhruv <dhruv.madderlawar24@vit.edu>
2026-09-01 00:14:05 +0530

    Developed and integrated theme_provider.dart into app routing

---
+++ lib/providers/theme_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final themeProvider = StateNotifierProvider<ThemeNotifier, ThemeMode>((ref) {
+  return ThemeNotifier();
+});
+
+class ThemeNotifier extends StateNotifier<ThemeMode> {
+  static const _themeKey = 'theme_mode';
+
+  ThemeNotifier() : super(ThemeMode.system) {
+    _loadTheme();
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final index = prefs.getInt(_themeKey);
+    if (index != null && index >= 0 && index < ThemeMode.values.length) {
+      state = ThemeMode.values[index];
+    }
+  }
+
+  Future<void> setTheme(ThemeMode mode) async {
+    state = mode;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(_themeKey, mode.index);
+  }
+}
